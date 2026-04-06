e21e334aaad902772d3a1f53aa83fb5a7b7f493b
Marcos Dias <marcosaurelioce@hotmail.com>
2026-04-06 12:38:18 -0300

    chore: initialize spring boot project foundation

---
+++ src/test/java/com/marcosdias/orderflowapi/OrderFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosdias.orderflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFlowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
